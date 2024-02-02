bba29648ec7c29a27a3be0adbb3c8851e78570a3
sofffja <sofi.efron@gmail.com>
2024-02-02 11:21:34 -0300

    Add basic lists functionality

--- src/index.js
+++ src/index.js
@@ -2,33 +2,70 @@ import './style.css'
 import Todo from './todoItem'
 import List from './list.js'
 
-const defaultList = new List('default');
+const lists = function() {
+  const listsArray = [];
+  let currentList;
+  
+  const add = (title) => {
+    listsArray.push(new List(title));
+  }
 
-for (let i = 1; i <= 5; i++) {
-  const defaultTodo = new Todo(`task ${i}`, `description ${i}`, '19-02-24', 1);
-  defaultList.todos.push(defaultTodo);
-}
+  const getCurrent = () => currentList;
 
+  const setCurrent = (index) => {
+    currentList = listsArray[index];
+  }
+
+  return { listsArray, getCurrent, setCurrent, add }
+}();
 
 const todos = function() {
   function add(title, description, dueDate, priority) {
-    defaultList.todos.push(new Todo(title, description, dueDate, priority));
+    lists.getCurrent().todos.push(new Todo(title, description, dueDate, priority));
     DOMHandler.displayTodos();
   }
 
   return { add }
 }();
 
+const initDefault = function() {
+  lists.add('Home');
+  lists.setCurrent(0);
+
+  for (let i = 1; i <= 5; i++) {
+    const defaultTodo = new Todo(`task ${i}`, `description ${i}`, '19-02-24', 1);
+    lists.getCurrent().todos.push(defaultTodo);
+  }
+}();
+
 const DOMHandler = function() {
   const todosDiv = document.querySelector('#todos');
   const listsDiv = document.querySelector('#lists');
+  const currentListH1 = document.querySelector('#current-list');
+  const newListBtn = document.querySelector('.new-list');
   displayTodos();
+  displayLists();
+  
+  function displayLists() {
+    listsDiv.textContent = '';
+    for (const list of lists.listsArray) {
+      const listPara = document.createElement('p');
+      listPara.textContent = list.title;
+      listPara.setAttribute('index', lists.listsArray.indexOf(list));
+      listsDiv.appendChild(listPara);
+
+      listsDiv.addEventListener('click', (e) => {
+        let index = e.target.getAttribute('index');
+        lists.setCurrent(index);
+      })
+    }
+  };
 
   function displayTodos() {
     todosDiv.textContent = '';
-    for (const todo of defaultList.todos) {
+    for (const todo of lists.getCurrent().todos) {
       const newItem = createTodo(todo);
-      newItem.setAttribute('index', defaultList.todos.indexOf(todo));
+      newItem.setAttribute('index', lists.getCurrent().todos.indexOf(todo));
       todosDiv.appendChild(newItem)
     }
   };
@@ -58,7 +95,7 @@ const DOMHandler = function() {
     deleteTodo.textContent = 'delete';
     deleteTodo.addEventListener('click', (e) => {
       let index = e.target.closest('[index]')
-      defaultList.todos.splice(index, 1);
+      lists.getCurrent().todos.splice(index, 1);
       displayTodos();
     })
 
@@ -70,6 +107,14 @@ const DOMHandler = function() {
 
     return newItem;
   }
+
+  const eventsHandler = function() {
+    newListBtn.addEventListener('click', (e) => {
+      let title = prompt('list title?', 'New list')
+      lists.add(title);
+      displayLists();
+    })
+  }();
 
   return { displayTodos }
 }();
